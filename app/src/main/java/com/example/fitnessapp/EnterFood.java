c99819bad17f0684367099513c2d33def0117ef5
MichaelWoj <wojnowskimichael@gmail.com>
2023-07-27 22:17:05 +0100

    Changing from ListLayout to RecyclerLayout

--- app/src/main/java/com/example/fitnessapp/EnterFood.java
+++ app/src/main/java/com/example/fitnessapp/EnterFood.java
@@ -1,39 +1,63 @@
 package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EnterFood extends AppCompatActivity {
 
-     ListView lv_foodList;
-     ArrayAdapter foodArrayAdapter;
-     DataBaseHelper dataBaseHelper;
+    RecyclerView recyclerView;
+    ArrayList<String> foodName, foodCaloriesNum, foodFatNum, foodCarbsNum, foodProteinNum;
+    DataBaseHelper dataBaseHelper;
+    FdbRecycleViewAdapter adapter;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seach_food);
-
-        lv_foodList = findViewById(R.id.lv_foodList);
-
         dataBaseHelper = new DataBaseHelper(EnterFood.this);
 
-        ShowFoodOnRecycleView();
+        foodName = new ArrayList<>();
+        foodCaloriesNum = new ArrayList<>();
+        foodFatNum = new ArrayList<>();
+        foodCarbsNum = new ArrayList<>();
+        foodProteinNum = new ArrayList<>();
 
-
+        recyclerView = findViewById(R.id.recyclerViewFoodList);
+        adapter = new FdbRecycleViewAdapter(this, foodName, foodCaloriesNum, foodFatNum, foodCarbsNum, foodProteinNum);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        displayData();
     }
 
-    private void ShowFoodOnRecycleView() {
-        foodArrayAdapter = new ArrayAdapter<FoodModel>(EnterFood.this, android.R.layout.simple_list_item_1, dataBaseHelper.getAllFoods());
-        lv_foodList.setAdapter(foodArrayAdapter);
+    private void displayData() {
+        Cursor cursor = (Cursor) dataBaseHelper.getAllFoods();
+        if(cursor.getCount()==0){
+            Toast.makeText(EnterFood.this, "No Entry Found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            while (cursor.moveToNext()){
+                foodName.add(cursor.getString(0));
+                foodCaloriesNum.add(cursor.getString(1));
+                foodFatNum.add(cursor.getString(2));
+                foodCarbsNum.add(cursor.getString(3));
+                foodProteinNum.add(cursor.getString(4));
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/fitnessapp/FdbRecycleViewAdapter.java
+++ app/src/main/java/com/example/fitnessapp/FdbRecycleViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.fitnessapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,40 +9,55 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FdbRecycleViewAdapter extends RecyclerView.Adapter<FdbRecycleViewAdapter.MyViewHolder> {
+import java.util.ArrayList;
 
+public class FdbRecycleViewAdapter extends RecyclerView.Adapter<FdbRecycleViewAdapter.MyViewHolder> {
+    private Context context;
+    private ArrayList rvDBFoodName, rvDBCaloriesNum, rvDBFatNum, rvDBCarbsNum , rvDBProteinNum;
+
+    public FdbRecycleViewAdapter(Context context, ArrayList rvDBFoodName, ArrayList rvDBCaloriesNum, ArrayList rvDBFatNum, ArrayList rvDBCarbsNum, ArrayList rvDBProteinNum){
+        this.context = context;
+        this.rvDBFoodName = rvDBFoodName;
+        this.rvDBCaloriesNum = rvDBCaloriesNum;
+        this.rvDBFatNum = rvDBFatNum;
+        this.rvDBCarbsNum = rvDBCarbsNum;
+        this.rvDBProteinNum = rvDBProteinNum;
+    }
 
     @NonNull
     @Override
-    public FdbRecycleViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.recycler_view_row,parent,false);
+        return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FdbRecycleViewAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.rvDBFoodName.setText(String.valueOf(rvDBFoodName.get(position)));
+        holder.rvDBCaloriesNum.setText(String.valueOf(rvDBCaloriesNum.get(position)));
+        holder.rvDBFatNum.setText(String.valueOf(rvDBFatNum.get(position)));
+        holder.rvDBCarbsNum.setText(String.valueOf(rvDBCarbsNum.get(position)));
+        holder.rvDBProteinNum.setText(String.valueOf(rvDBProteinNum.get(position)));
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return rvDBFoodName.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
-        TextView rvDBFoodName, rvDBCalories, rvDBCaloriesNum, rvDBFat, rvDBFatNum, rvDBCarbs, rvDBCarbsNum , rvDBProtein, rvDBProteinNum;
+        TextView rvDBFoodName, rvDBCaloriesNum, rvDBFatNum, rvDBCarbsNum , rvDBProteinNum;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             rvDBFoodName = itemView.findViewById(R.id.recyclerViewFoodName);
-            rvDBCalories = itemView.findViewById(R.id.recyclerViewCalories);
             rvDBCaloriesNum = itemView.findViewById(R.id.recyclerViewCaloriesNum);
-            rvDBFat = itemView.findViewById(R.id.recyclerViewFat);
             rvDBFatNum = itemView.findViewById(R.id.recyclerViewFatNum);
-            rvDBCarbs = itemView.findViewById(R.id.recyclerViewCarbs);
             rvDBCarbsNum = itemView.findViewById(R.id.recyclerViewCarbsNum);
-            rvDBProtein = itemView.findViewById(R.id.recyclerViewProtein);
             rvDBProteinNum = itemView.findViewById(R.id.recyclerViewProteinNum);
         }
     }
