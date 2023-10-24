2b99a7d328cc16f3d9e4bd0a3ecc7610874cc03b
MichaelWoj <wojnowskimichael@gmail.com>
2023-10-24 15:36:19 +0100

    Manual add food name only has to be added if the food is being saved

--- app/src/main/java/com/example/fitnessapp/ManuallyAdd.java
+++ app/src/main/java/com/example/fitnessapp/ManuallyAdd.java
@@ -1,39 +1,69 @@
 package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.KeyListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ManuallyAdd extends AppCompatActivity {
 
     private EditText foodName, foodCalories, foodFat, foodCarbs, foodProtein;
+    private TextView tvMealName;
     private Button submit, back;
     private Switch db_switch;
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manually_add);
 
+        tvMealName = findViewById(R.id.mealName);
         foodName = findViewById(R.id.manualMealName);
         foodCalories = findViewById(R.id.manualMealCalories);
         foodFat = findViewById(R.id.manualMealFat);
         foodCarbs = findViewById(R.id.manualMealCarbs);
         foodProtein = findViewById(R.id.manualMealProtein);
         db_switch = findViewById(R.id.saveToDBSwitch);
-
         submit = findViewById(R.id.manualAddFood);
+
+        tvMealName.setTextColor(ContextCompat.getColor(this, R.color.gray));
+        foodName.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.gray));
+
+        KeyListener keyListener = foodName.getKeyListener();
+        foodName.setKeyListener(null);
+        db_switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (db_switch.isChecked()){
+                    foodName.setKeyListener(keyListener);
+                    foodName.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.black));
+                    tvMealName.setTextColor(ContextCompat.getColor(ManuallyAdd.this, R.color.black));
+                }
+                else{
+                    foodName.setKeyListener(null);
+                    tvMealName.setTextColor(ContextCompat.getColor(ManuallyAdd.this, R.color.gray));
+                    foodName.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.gray));
+                }
+            }
+        });
+
         submit.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View view) {
 
-                    if (foodName.getText().toString().isEmpty()||foodCalories.getText().toString().isEmpty() || foodFat.getText().toString().isEmpty() || foodCarbs.getText().toString().isEmpty() || foodProtein.getText().toString().isEmpty()){
+                    if (foodCalories.getText().toString().isEmpty() || foodFat.getText().toString().isEmpty() || foodCarbs.getText().toString().isEmpty() || foodProtein.getText().toString().isEmpty()) {
+                        Toast.makeText(ManuallyAdd.this, "Please insert all info", Toast.LENGTH_SHORT).show();
+                    }else if (db_switch.isChecked() && foodName.getText().toString().isEmpty()){
                         Toast.makeText(ManuallyAdd.this, "Please insert all info", Toast.LENGTH_SHORT).show();
                     }else{
 
